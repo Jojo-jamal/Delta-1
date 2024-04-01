cbde39268051e1cfb65e72a80e9837f086e45b02
Jojo-jamal <165210037+Jojo-jamal@users.noreply.github.com>
2024-04-01 20:33:39 +0200

    Test.py

--- Test.py
+++ Test.py
@@ -1,4 +1,5 @@
 import webbrowser
+from et_xmlfile import xmlfile
 from openpyxl import load_workbook
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
@@ -7,27 +8,37 @@ from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
 from kivy.uix.popup import Popup
 from kivy.uix.scrollview import ScrollView
-
-
+import xlsxwriter
+from kivy.uix.image import Image
+from kivy.metrics import inch
 class DeltaPopLocaties(BoxLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.cols = 1
+        self.inside = BoxLayout()
+        self.inside.cols = 3
         self.orientation = "vertical"
-        self.label_search = Label(text="Type de Naam van jou gewenste pop:")
+
+        def on_size(self, instance, value):
+            # run parent on_size manually
+            super(TextInput, self).on_size(instance, value)
+            self.font_size = self.height * 0.8
+
+        self.label_search = Label(text='Type de Naam van jou gewenste pop \n Bijvoorbeeld AB-02\n Hou de zoek knopje ingedrukt:')
         self.add_widget(self.label_search)
 
-        self.entry_search = TextInput()
-        self.add_widget(self.entry_search)
+        self.entry_search = TextInput(multiline=False)
+        self.inside.add_widget(self.entry_search)
 
         self.button_search = Button(text="Zoeken")
         self.button_search.bind(on_press=self.search_excel)
-        self.add_widget(self.button_search)
-
+        self.inside.add_widget(self.button_search)
+        self.add_widget(self.inside)
     def search_excel(self, instance):
         search_query = self.entry_search.text
         if not search_query:
-            popup = Popup(title='Error', content=Label(text='A.U.B Type de naam van de pop.'), size_hint=(None, None),
-                          size=(400, 200))
+            popup = Popup(title='Error', content=Label(text='A.U.B Type de naam van de pop.', multiline=False), size_hint=(None, None),
+                          size=(400, 400))
             popup.open()
             return
 
@@ -49,22 +60,44 @@ class DeltaPopLocaties(BoxLayout):
                         result_dict[title] = result[i]
 
                 result_str = "\n".join([f"{key}: {value}" for key, value in result_dict.items()])
-                popup = Popup(title='Search Result', content=ScrollView(size_hint=(1, None), size=(400, 400)),
-                              size_hint=(None, None), size=(400, 400))
+                popup = Popup(title='Search Result', content=ScrollView(size_hint=(2,None), size=(600, 600)),
+                              size_hint=(1, None), size=(800, 400))
                 popup.content.add_widget(Label(text=result_str))
                 popup.open()
 
                 # Add hyperlink button
                 if 'Adres' in result_dict:
                     address = result_dict['Adres']
-                    hyperlink_button = Button(text=f"Open Google Maps", size_hint=(None, None),
-                                              size=(400, 50))
+                    hyperlink_button = Button(text=f"Open Google Maps",size_hint=(None, None),
+                                              size=(800, 700))
                     hyperlink_button.bind(on_press=lambda instance: self.open_google_maps(address))
                     # Wrap the button in a BoxLayout to center it horizontally
-                    button_layout = BoxLayout(orientation='horizontal', size_hint=(None, None), size=(400, 50))
+                    button_layout = BoxLayout(orientation='horizontal',size_hint=(None, None), size=(400, 100))
                     button_layout.add_widget(Label())  # Empty label to push the button to the center
                     button_layout.add_widget(hyperlink_button)
                     self.add_widget(button_layout)
+
+            else:
+                popup = Popup(title='Search Result', content=Label(text='Search query not found.'),
+                              size_hint=(None, None), size=(700, 400))
+                popup.open()
+        except FileNotFoundError:
+            popup = Popup(title='Error', content=Label(text='Excel file not found.'), size_hint=(None, None),
+                          size=(600, 200))
+            popup.open()
+
+    def open_google_maps(self, address):
+        if address:
+            hyperlink = f"https://www.google.com/maps/search/{address}"
+            webbrowser.open(hyperlink)
+            return Image(source='log.jpg')
+class ExcelLocationSearchApp(App):
+    def build(self):
+        return DeltaPopLocaties()
+
+if __name__ == "__main__":
+    ExcelLocationSearchApp().run()
+                  self.add_widget(button_layout)
 
             else:
                 popup = Popup(title='Search Result', content=Label(text='Search query not found.'),
